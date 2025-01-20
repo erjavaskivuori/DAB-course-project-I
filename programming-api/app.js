43aec881cf5dec1e6440c6d13cc0fcafbf99bae3
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-20 21:36:52 +0200

    added url mapping, functions to get root and to get all assignments

--- programming-api/app.js
+++ programming-api/app.js
@@ -1,10 +1,21 @@
 import * as programmingAssignmentService from "./services/programmingAssignmentService.js";
 import { serve } from "./deps.js";
 import { sql } from "./database/database.js";
+import { cacheMethodCalls } from "./util/cacheUtil.js";
 
-const handleRequest = async (request) => {
-  const programmingAssignments = await programmingAssignmentService.findAll();
+const cachedProgrammingAssignmentService = cacheMethodCalls(programmingAssignmentService, ["findAll"]);
 
+const SERVER_ID = crypto.randomUUID();
+
+const handleGetRoot = async (request) => {
+  return new Response(`Hello from ${ SERVER_ID }`);
+};
+
+const handleGetAssignments = async ( request ) => {
+  return Response.json(await cachedProgrammingAssignmentService.findAll());
+};
+
+const handlePostAssignment = async ( request ) => {
   const requestData = await request.json();
   const testCode = programmingAssignments[0]["test_code"];
   const data = {
@@ -21,6 +32,41 @@ const handleRequest = async (request) => {
   });
 
   return response;
+}
+
+const urlMapping = [
+  {
+    pattern: new URLPattern({ pathname: "/" }),
+    method: "GET",
+    fn: handleGetRoot,
+  },
+  {
+    pattern: new URLPattern({ pathname: "/assignments" }),
+    method: "GET",
+    fn: handleGetAssignments,
+  },
+  {
+    pattern: new URLPattern({ pathname: "/grade" }),
+    method: "POST",
+    fn: handlePostAssignment,
+  }
+]
+
+const handleRequest = async (request) => {
+  const mapping = urlMapping.find(
+    (um) => um.method === request.method && um.pattern.test(request.url)
+  );
+
+  if (!mapping) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const mappingResult = mapping.pattern.exec(request.url);
+  try {
+    return await mapping.fn(request, mappingResult);
+  } catch (e) {
+    return new Response(e.stack, { status: 500 })
+  }
 };
 
 const portConfig = { port: 7777, hostname: "0.0.0.0" };
