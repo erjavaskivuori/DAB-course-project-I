cc3b9c844d7291ce9ecdc7769fa06be9f691cf51
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-22 23:00:18 +0200

    oopsie huge updates to grading, finding pending submissions, updating submission info aaaand to fetch submission info

--- programming-api/app.js
+++ programming-api/app.js
@@ -1,11 +1,41 @@
 import * as programmingAssignmentService from "./services/programmingAssignmentService.js";
 import { serve } from "./deps.js";
-import { sql } from "./database/database.js";
 import { cacheMethodCalls } from "./util/cacheUtil.js";
+import { connect } from "./deps.js";
 
-const cachedProgrammingAssignmentService = cacheMethodCalls(programmingAssignmentService, ["findAll"]);
+const cachedProgrammingAssignmentService = cacheMethodCalls(programmingAssignmentService, []);
 
 const SERVER_ID = crypto.randomUUID();
+const redis = await connect({
+  hostname: "redis",
+  port: 6379,
+});
+
+const parseFeedback = (graderFeedback) => {
+  let correct = false;
+  let feedback = {
+    explanation: "OK",
+    output: ""
+  };
+
+  if (graderFeedback.includes("AssertionError")) {
+    feedback.explanation = "Program returned incorrect value";
+  } else if (graderFeedback.includes("SyntaxError")) {
+    feedback.explanation = "Syntax Error";
+  } else if (graderFeedback.includes("NameError")) {
+    feedback.explanation = "Name Error: Function not defined correctly";
+  } else if (graderFeedback.includes("Time out error")) {
+    feedback.explanation = "Time out error";
+  } else if (graderFeedback.includes("OK")) {
+    correct = true;
+  } else {
+    feedback.explanation = "Unknown error";
+  }
+
+  feedback.output = graderFeedback;
+
+  return { correct, feedback };
+};
 
 const handleGetRoot = async (request) => {
   return new Response(`Hello from ${ SERVER_ID }`);
@@ -18,22 +48,122 @@ const handleGetAssignments = async ( request ) => {
 const handlePostAssignment = async ( request ) => {
   const programmingAssignments = await cachedProgrammingAssignmentService.findAll();
   const requestData = await request.json();
-  const testCode = programmingAssignments[0]["test_code"];
+  const testCode = programmingAssignments[requestData.assignment-1]["test_code"];
   const data = {
+    userId: requestData.user,
+    assignmentId: requestData.assignment,
     testCode: testCode,
     code: requestData.code,
   };
 
-  const response = await fetch("http://grader-api:7000/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  const submission = await cachedProgrammingAssignmentService.addSubmission(
+    data.assignmentId,
+    data.code,
+    data.userId,
+    "pending",
+    "",
+    false,
+    new Date().toISOString()
+  );
 
-  return response;
-}
+  await redis.xadd(
+    "submission-stream",
+    "*",
+    { user: data.userId, id: submission[0].id, code: data.code, testCode: testCode },
+  );
+
+  const feedbackData = {
+    id: submission[0].id,
+    correct: false,
+    feedback: {
+      explanation: "",
+      output: "",
+    },
+    status: "pending",
+  };
+
+  return Response.json(feedbackData);
+};
+ 
+const handlePostSubmissionPending = async (request) => {
+  const searchParams = await request.json();
+  const user = searchParams.user;
+
+  let lastId = "0";
+  try {
+    while (true) {
+      const [submissionStream] = await redis.xreadgroup( // xreadgroup = messages that are not yet acknowledged
+        [{ key: "submission-stream", xid: lastId }],
+        { group: "Redis-Submission-Group", consumer: "Submission-Consumer", count: 10 }
+      );
+
+      if (!submissionStream || submissionStream.messages.length === 0) {
+        break;
+      }
+
+      for (const message of submissionStream.messages) {
+        if (message.fieldValues.user === user) {
+          const id = message.fieldValues.id;
+          return new Response(JSON.stringify({ pending: true, id: id }), {
+            headers: { "Content-Type": "application/json" },
+          });
+        }
+      }
+
+      lastId = submissionStream.messages[submissionStream.messages.length - 1].xid;
+    }
+
+    return new Response(JSON.stringify({ pending: false }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    console.error("Error checking user in stream:", e);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
+
+const handlePostSubmissionUpdate = async (request) => {
+  const gradedSubmission = await request.json();
+  const parsedOutput = parseFeedback(gradedSubmission.feedback);
+  try {
+    const feedbackJson = JSON.stringify(parsedOutput.feedback);
+
+    await programmingAssignmentService.updateSubmission(
+      gradedSubmission.id,
+      "processed",
+      feedbackJson,
+      parsedOutput.correct
+    );
+    return new Response(JSON.stringify({ success: true }));
+  } catch (e) {
+    console.error("Error updating submission status:", e);
+    return new Response(JSON.stringify({ success: false }));
+  }
+};
+
+const handleGetSubmission = async (request) => {
+  try {
+    const searchParams = await request.json();
+    const submission = await programmingAssignmentService.findBySubmissionId(searchParams.id);
+    const data = {
+      status: submission[0].status,
+      feedback: JSON.parse(submission[0].grader_feedback),
+      correct: submission[0].correct,
+    };
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    console.error("Error fetching submission:", e);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
 
 const urlMapping = [
   {
@@ -50,8 +180,23 @@ const urlMapping = [
     pattern: new URLPattern({ pathname: "/grade" }),
     method: "POST",
     fn: handlePostAssignment,
+  },
+  {
+    pattern: new URLPattern({ pathname: "/pending-submission" }),
+    method: "POST",
+    fn: handlePostSubmissionPending,
+  },
+  {
+    pattern: new URLPattern({ pathname: "/update-submission" }),
+    method: "POST",
+    fn: handlePostSubmissionUpdate,
+  },
+  {
+    pattern: new URLPattern({ pathname: "/submission" }),
+    method: "POST",
+    fn: handleGetSubmission,
   }
-]
+];
 
 const handleRequest = async (request) => {
   const mapping = urlMapping.find(
--- programming-api/services/programmingAssignmentService.js
+++ programming-api/services/programmingAssignmentService.js
@@ -4,4 +4,25 @@ const findAll = async () => {
   return await sql`SELECT * FROM programming_assignments;`;
 };
 
-export { findAll };
+const addSubmission = async (programmingAssignmentId, code, userId) => {
+  return await sql`
+  INSERT INTO programming_assignment_submissions (programming_assignment_id, code, user_uuid) 
+  VALUES (${programmingAssignmentId}, ${code}, ${userId}) RETURNING id`;
+};
+
+const updateSubmission = async (submissionId, status, feedback, correct) => {
+  await sql`
+  UPDATE programming_assignment_submissions 
+  SET status = ${status}, grader_feedback = ${feedback}, correct = ${correct} 
+  WHERE id = ${submissionId}`;
+};
+
+const findBySubmissionId = async (submissionId) => {
+  return await sql`
+    SELECT status, grader_feedback, correct
+    FROM programming_assignment_submissions
+    WHERE id = ${submissionId};
+  `;
+}
+
+export { findAll, addSubmission, updateSubmission, findBySubmissionId };
