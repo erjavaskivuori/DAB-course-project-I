feb1d87660c3bdead4d2883ebad3b65187e84055
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-22 22:51:39 +0200

    changed handling method to a redis stream where the submissions can be queued

--- grader-api/app.js
+++ grader-api/app.js
@@ -1,24 +1,54 @@
-import { serve } from "./deps.js";
 import { grade } from "./services/gradingService.js";
+import { connect } from "./deps.js";
 
-const handleRequest = async (request) => {
-  let result;
-  try {
-    const requestData = await request.json();
+const redis = await connect({
+  hostname: "redis",
+  port: 6379,
+});
 
-    console.log("Request data:");
-    console.log(requestData);
-
-    const code = requestData.code;
-    const testCode = requestData.testCode;
-
-    result = await grade(code, testCode);
-  } catch (e) {
-    result = await gradingDemo();
-  }
-
-  return new Response(JSON.stringify({ result: result }));
+try {
+  const redisInfo = await redis.xinfoGroups("submission-stream");
+  console.log(`${redisInfo[0].name} group exists`);
+} catch (e) {
+  console.log("Redis group does not exist, creating it now");
+  await redis.xgroupCreate(
+    "submission-stream",
+    "Redis-Submission-Group",
+    0,
+    true,
+  );
 };
 
-const portConfig = { port: 7000, hostname: "0.0.0.0" };
-serve(handleRequest, portConfig);
+while(true) {
+
+  const [submissionStream] = await redis.xreadgroup(
+    [{ key: "submission-stream", xid: ">" }],
+    { group: "Redis-Submission-Group", consumer: "Submission-Consumer", count: 1 },
+  );
+  if (submissionStream) {
+    const code = submissionStream.messages[0].fieldValues.code
+    const testCode = submissionStream.messages[0].fieldValues.testCode
+    const result = await grade(code, testCode);
+
+    const data = {
+      id: submissionStream.messages[0].fieldValues.id,
+      feedback: result,
+      };
+     const programmingApiResponse = await fetch("http://programming-api:7777/update-submission", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      await programmingApiResponse.json();
+
+    await redis.xack(
+      "submission-stream",
+      "Redis-Submission-Group",
+      submissionStream.messages[0].xid.unixMs,
+    );
+  };
+
+  await new Promise(r => setTimeout(r, 500));
+};
