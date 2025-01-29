02cb90005096a7a5ff511f54596e761ed96e0228
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-29 20:10:38 +0200

    updated ui design

--- programming-ui/src/components/GradingButton.svelte
+++ programming-ui/src/components/GradingButton.svelte
@@ -67,37 +67,54 @@
 </script>
 
 <div class="w-screen min-h-screen bg-gray-900">
-  <button
-    class="font-bold p-4 rounded m-10 text-white"
-    class:bg-blue-500={!pending}
-    class:hover:bg-blue-700={!pending}
-    class:bg-gray-500={pending}
-    on:click={submitToGrading}
-    disabled={pending}
-  >
-    Submit for grading
-  </button>
+  <div class="mx-2">
+    <button
+      class="font-bold p-4 rounded my-10 text-white"
+      class:bg-blue-500={!pending}
+      class:hover:bg-blue-700={!pending}
+      class:bg-gray-500={pending}
+      on:click={submitToGrading}
+      disabled={pending}
+    >
+      Submit for grading
+    </button>
 
-  {#if pending}
-    <p class="text-yellow-500">Grading in progress...</p>
+    {#if pending}
+      <p class="text-xl text-yellow-400">Grading in progress...</p>
 
-  {/if}
-
-  {#if gradingResult && !pending}
-    {#if gradingResult.correct}
-      <p class="text-green-500">Your submission is correct!</p>
-      <button
-        class="bg-green-500 hover:bg-green-700 text-white font-bold p-4 rounded m-10"
-        on:click={moveToNextAssignment}
-      >
-        Move to next assignment
-      </button>
-    {:else}
-      <p class="text-red-500">Your submission is incorrect. Feedback: {gradingResult.feedback.explanation}</p>
-      <div>
-        <h3 class="text-2xl my-4">Output:</h3>
-        <pre>{gradingResult.feedback.output}</pre>
-      </div>
     {/if}
-  {/if}
+
+    {#if gradingResult && !pending}
+      {#if gradingResult.correct}
+        <p class="text-xl text-green-500">Your submission is correct!</p>
+        <button
+          class="bg-green-500 hover:bg-green-700 text-white font-bold p-4 rounded my-10 inline-flex items-center"
+          on:click={moveToNextAssignment}
+        >
+          Move to next assignment
+          <svg
+            class="ms-2 h-5 w-6 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+      {:else}
+        <p class="text-xl text-red-500">Your submission is incorrect. Feedback: {gradingResult.feedback.explanation}</p>
+        <div>
+          <h3 class="text-2xl my-4">Output:</h3>
+          <pre>{gradingResult.feedback.output}</pre>
+        </div>
+      {/if}
+    {/if}
+  </div>
 </div>
