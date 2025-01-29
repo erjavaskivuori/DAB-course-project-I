f9bd20b84c70624ddc1cc0054737012039a90f00
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-29 22:14:31 +0200

    ui updates

--- programming-ui/src/layout/Header.svelte
+++ programming-ui/src/layout/Header.svelte
@@ -2,6 +2,6 @@
   import { userUuid } from "../stores/stores.js";
 </script>
 
-<nav class="p-4 bg-gray-900">
+<nav class="w-full flex flex-col items-center pt-7 pb-10 bg-gray-900">
   <span class="text-2xl text-gray-400 font-poppins">Hello {$userUuid}!</span>
 </nav>
