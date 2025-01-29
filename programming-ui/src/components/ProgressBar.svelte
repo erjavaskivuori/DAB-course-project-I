4eb74c69ed20db9d174a82c281a2aa04289548d4
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-29 22:12:54 +0200

    created progress bar

---
+++ programming-ui/src/components/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let solvedAssignments;
+  export let assignments;
+
+  let progress = 0;
+  let points = 0;
+  let maxPoints = 0;
+  let allSolved = false;
+
+  $: if (assignments && assignments.length > 0) {
+    progress = solvedAssignments.length / assignments.length * 100;
+    points = solvedAssignments.length * 100;
+    maxPoints = assignments.length * 100;
+  }
+</script>
+
+<div class="my-8 max-w-screen">
+  <h1 class="my-7 text-xl font-bold">
+    Your progress: {points} / {maxPoints} points
+  </h1>
+  <div class="bg-gray-200 rounded-full h-4 dark:bg-gray-700">
+    <div class="bg-green-600 h-4 rounded-full" style="width: {progress}%"></div>
+  </div>
+</div>
