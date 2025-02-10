49257c94ee4336a3fe84e0c080d7244432cda59e
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-02-10 20:23:23 +0200

    created tests

---
+++ e2e-playwright/tests/programming_tests.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require("@playwright/test");
+
+test("Server responds with a page with the title 'Programming assignments'", async ({ page }) => {
+  await page.goto("/");
+  expect(await page.title()).toBe("Programming assignments");
+});
+
+test("Incorrect submission fails tests and correct feedback is returned", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("[id='code_editor']").fill("failing value");
+  await page.click("button:has-text('Submit for grading')");
+  await page.waitForSelector("text=Your submission is incorrect. Feedback: Syntax Error");
+});
+
+test("Correct submission passes tests and correct feedback is returned", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("[id='code_editor']").fill("def hello():\n  return 'Hello'");
+  await page.click("button:has-text('Submit for grading')");
+  expect(await page.waitForSelector("text=Your submission is correct!"));
+});
+
+test("After a correct submission, can move to the next assignment", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("[id='code_editor']").fill("def hello():\n  return 'Hello'");
+  await page.click("button:has-text('Submit for grading')");
+  await page.waitForSelector("text=Your submission is correct!")
+  await page.click("button:has-text('Move to next assignment')");
+  expect(await page.waitForSelector("[id='assignment_2']", {timeout: 5000}));
+});
+
+test("After a correct submission user's points change", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("[id='code_editor']").fill("def hello():\n  return 'Hello'");
+  await page.click("button:has-text('Submit for grading')");
+  await page.waitForSelector("text=Your submission is correct!")
+  expect(page.locator("[id='progress']").locator("text=100")).toBeVisible();
+});
